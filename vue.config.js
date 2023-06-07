99a7637eb4fe199c0864223c95daedc08f87af4b
an_an <1530765818@qq.com>
2023-06-07 16:12:20 +0800

    feat: 引入 normalize.css，引入less，less-loader，配置 additionalData

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
     transpileDependencies: true,
+
+    css: {
+        loaderOptions: {
+            less: {
+                additionalData: `
+                    @import "@/assets/css/index.less";`,
+            },
+        },
+    },
+
     configureWebpack: {
         resolve: { extensions: ['.ts', '.tsx', '.js', '.json', '.vue'] }, // 默认导入文件目录下的 index.* 这些文件
     },
