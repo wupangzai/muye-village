11b4282ec25549a9a6d3f6e5345b9c5c66481352
an_an <1530765818@qq.com>
2023-06-26 17:11:18 +0800

    fix: 将自定义指令中的 any 替换成对应的类型声明

--- src/directives/tooltip/index.ts
+++ src/directives/tooltip/index.ts
@@ -1,4 +1,9 @@
+import { DirectiveBinding, VueConstructor, VNode, VNodeDirective } from 'vue';
 import ToolTip from './index.vue';
+
+interface El extends HTMLBaseElement {
+    instance: InstanceType<VueConstructor>;
+}
 
 /**
  * 用于给指定元素/组件绑定一个 tooltip 提示, 可通过第一个修饰符来指定 tooltip 的呼出位置，eg: v-tooltip.bottom
@@ -6,10 +11,9 @@ import ToolTip from './index.vue';
  * <div v-tooltip="'1'">
  *   <div v-tooltip="'2'"></div>
  * </div>
- * TODO FIXME 该文件any替换成对应的类型
  */
 export default {
-    bind(el: any, binding: any, vnode: any) {
+    bind(el: El, binding: DirectiveBinding<{}>, vnode: VNode) {
         const modifiersKey = Object.keys(binding.modifiers);
         const instanceToolTip = new ToolTip({
             propsData: {
@@ -18,16 +22,17 @@ export default {
             },
         });
 
-        let vnodeDirectives = vnode.data.directives;
+        let vnodeDirectives = vnode!.data!.directives;
 
         // 一般在内部使用，需要过滤掉自身 tooltip 指令，防止爆栈
-        vnode.data.directives = vnodeDirectives.filter(
-            (directive: any) => directive.name !== 'tooltip',
+        vnode!.data!.directives = vnodeDirectives!.filter(
+            (directive: VNodeDirective) => directive.name !== 'tooltip',
         );
 
         // 下面这种方法我也不知道为啥就可以了，试出来的
         // 给实例组件的 tooltip 内容插槽插入 该el
-        instanceToolTip.$slots.tooltip = vnode;
+        // 这里类型是 vnode[],不知道为啥 TODO FIXME
+        instanceToolTip.$slots.tooltip = vnode as unknown as VNode[];
 
         // 然后渲染，当该 el 被 hover 的时候就自动会 append 到 body
         const renderInstance = instanceToolTip.$mount();
@@ -35,7 +40,7 @@ export default {
         el.instance = renderInstance;
     },
 
-    unbind(el: any, binding: any, vnode: any) {
+    unbind(el: El, binding: DirectiveBinding<{}>, vnode: VNode) {
         el.instance && el.instance.$destroy();
         document.body.removeChild(el.instance.$el);
     },
