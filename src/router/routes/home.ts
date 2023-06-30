aa6029c13fc4e81b2f579b431524cc0946eddb13
an_an <1530765818@qq.com>
2023-06-30 21:32:06 +0800

    fix: 修复 由 tsc 编译后不是模块的问题，回退 home 路由至 import 导入

--- src/router/routes/home.ts
+++ src/router/routes/home.ts
@@ -2,7 +2,7 @@ export const homeRoutes = [
     {
         path: '/',
         name: 'Home',
-        component: require('@/views/home/index.vue').default, // 用import会报错，home.vue.ts is not a module
+        component: () => import('@/views/home/index.vue'), // 用import会报错，home.vue.ts is not a module
         meta: {
             permission: true,
         },
