852c6f72fa31297d919394abe1d499f7a530a401
unknown <181009@internet.com>
2023-08-04 17:28:50 +0800

    feat: 开发欢迎页面

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-container>
+        <Nav></Nav>
+    </el-container>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'AppWelcome',
+};
+</script>
+
+<script lang="ts" setup>
+import Nav from '@/components/nav/index.vue';
+</script>
+
+<style lang="less" scoped>
+.el-container {
+    width: 100%;
+    height: 100vh;
+    background-color: #000000;
+}
+</style>
