a1d800ee1cf076358b84a71abc0a4637ba01c4d2
an_an <1530765818@qq.com>
2023-08-10 10:26:11 +0800

    feat: 引入字体图标，完成 welcome 描述静态页面

--- src/components/welcome/welcome-description.vue
+++ src/components/welcome/welcome-description.vue
@@ -1,14 +1,30 @@
 <template>
     <div class="welcome-description">
         <div class="welcome-description-top">
-            <div class="welcome-description-title">Hire us for anything</div>
+            <div class="welcome-description-title">{{ title }}</div>
             <div class="welcome-description-sub-title">
-                as long as there's space for us to be creative!
+                {{ subTitle }}
             </div>
         </div>
-        <div class="welcome-description-middle">
-            That's it! No boring paragraphs of self boasting. We'll just list
-            out the services we do and let our works do rest of the talking.
+        <div class="welcome-description-middle">{{ middleTitle }}</div>
+        <div class="welcome-description-bottom">
+            <div
+                class="bottom-card-item"
+                v-for="(config, index) in cardConfigList"
+                :key="index"
+            >
+                <div class="icon-desc">
+                    <span :class="config.icon"></span>
+                    <span class="desc">{{ config.iconDesc }}</span>
+                </div>
+                <div
+                    class="bottom-card-list"
+                    v-for="(text, i) in config.textList"
+                    :key="i"
+                >
+                    <div class="text-list">{{ text }}</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -19,7 +35,32 @@ export default {
 };
 </script>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const title = 'Hire us for anything';
+
+const subTitle = "as long as there's space for us to be creative!";
+
+const middleTitle =
+    "That's it! No boring paragraphs of self boasting. We'll just list out the services we do and let our works do rest of the talking.";
+
+const cardConfigList = [
+    {
+        icon: 'icon-video',
+        iconDesc: 'Video Making',
+        textList: ['Ad Films', 'Corporate Films', 'Explainer Films'],
+    },
+    {
+        icon: 'icon-flag',
+        iconDesc: 'BRANDING',
+        textList: ['Brand Names', 'Branding Materials', 'Brand Guidelines'],
+    },
+    {
+        icon: 'icon-desktop',
+        iconDesc: 'WEB',
+        textList: ['UX Flow and UI Designing', 'Web designing', 'Wordpress'],
+    },
+];
+</script>
 
 <style lang="less" scoped>
 .welcome-description {
@@ -35,6 +76,47 @@ export default {
     &-middle {
         font-size: 16px;
         font-weight: 300;
+        margin-bottom: 15px;
+    }
+
+    &-bottom {
+        margin-top: 50px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .bottom-card-item {
+        width: 30%;
+    }
+
+    .bottom-card-list {
+        font-size: 14px;
+        // opacity: 0.6;
+        color: #aaa;
+        letter-spacing: 1px;
+        font-weight: 300;
+    }
+
+    .icon-desc {
+        font-size: 32px;
+        border-bottom: 1px solid #aaa;
+        padding-bottom: 15px;
+        color: #aaa;
+        font-weight: 300;
+        display: flex;
+        margin-bottom: 30px;
+
+        .desc {
+            padding-left: 10px;
+            font-size: 14px;
+            letter-spacing: 2px;
+            padding-top: 4px;
+        }
+    }
+
+    .text-list {
+        margin-bottom: 15px;
+        line-height: 24px;
     }
 
     &-title {
