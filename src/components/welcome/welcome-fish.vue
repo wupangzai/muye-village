37dd421eb345e412bc87d7ed9cf5090ee6fb39a4
WANG <181009@internet.com>
2023-08-11 17:04:18 +0800

    feat: 完成welcome-fish静态页面

--- src/components/welcome/welcome-fish.vue
+++ src/components/welcome/welcome-fish.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="welcome-fish">welcome-fish</div>
+    <div class="welcome-fish">
+        <div class="fish-elements">
+            <div
+                v-for="(item, index) in 12"
+                :key="index + 'one'"
+                class="el"
+                :class="setClass(index + 1)"
+            >
+                <img src="../../assets/img/fish.svg" />
+            </div>
+        </div>
+        <div class="fish-text">
+            <span
+                v-for="(item, index) in title"
+                :key="index"
+                :class="{ current: index == i }"
+            >
+                {{ item }}<strong v-show="index === 4">the brand</strong>
+            </span>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -8,10 +28,106 @@ export default {
 };
 </script>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { proxy: _this } = getCurrentInstance() as ComponentInternalInstance;
+
+const setClass = (index: number): string => {
+    return `el-${index}`;
+};
+
+const title: string[] = [
+    'Being one among everyone is boring. So,',
+    'we make you unique',
+    'we make you big',
+    'we make you stand apart',
+    'we make you ',
+];
+// 让字体循环变换
+let i: number = $ref(-1);
+const changeText = (): void => {
+    setTimeout((): void => {
+        if (i === -1) {
+            i++;
+        }
+    }, 0);
+    setInterval((): void => {
+        if (i < title.length - 1) {
+            i++;
+        } else {
+            i = 0;
+        }
+    }, 3000);
+};
+
+onMounted(() => {
+    changeText();
+});
+</script>
 
 <style lang="less" scoped>
 .welcome-fish {
-    height: calc(100vh - 50px); // 等 Nav fixed后需要调整
+    height: 100vh; // 等 Nav fixed后需要调整
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.fish-elements {
+    width: 500px;
+    height: 216px;
+    margin-bottom: 50px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    .el {
+        position: relative;
+        width: 85px;
+        height: 72px;
+        display: flex;
+        justify-content: center;
+        align-items: end;
+        margin: 0 7px;
+        transition: all 2s ease-in-out;
+
+        &-4 {
+            // translate: ;
+        }
+
+        &-6 {
+            margin-left: 14px;
+        }
+
+        img {
+            width: 40px;
+        }
+    }
+}
+
+.fish-text {
+    width: 100%;
+    height: 50px;
+    color: #ffffff;
+    position: relative;
+
+    span {
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        translate: -50% 0;
+        font-family: 'Lato', sans-serif;
+        transition: all 2s ease-in-out;
+        opacity: 0;
+
+        strong {
+            font-size: 16px;
+        }
+    }
+
+    .current {
+        opacity: 1;
+    }
 }
 </style>
