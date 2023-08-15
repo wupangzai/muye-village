7eef488f4cadac37f37ba495c7827b67aa64c892
WANG <181009@internet.com>
2023-08-15 15:45:48 +0800

    feat: 完成了鱼动画的第一次渲染

--- src/components/welcome/welcome-fish.vue
+++ src/components/welcome/welcome-fish.vue
@@ -7,7 +7,49 @@
                 class="el"
                 :class="setClass(index + 1)"
             >
-                <img src="../../assets/img/fish.svg" />
+                <img
+                    src="../../assets/img/fish.svg"
+                    :class="{ 'el-other-off': !showCommon }"
+                    v-if="index !== 3"
+                />
+                <svg
+                    ref="specialFish"
+                    class="special"
+                    style="enable-background·new 167.656 352.531 42 20;"
+                    viewBox="167.656 352.531 42 20"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    v-if="index === 3"
+                >
+                    <path
+                        id="fishBody"
+                        style="
+                            fill: none;
+                            stroke-miterlimit: 10;
+                            transition: all 2s ease 0s;
+                        "
+                        class="pre-unique"
+                        stroke-linejoin="round"
+                        xml:space="preserve"
+                        d="M205.093,362.893c0.137-0.366,2.803-0.554,2.803-0.554
+							s-13.138-18.904-29.079,0c0,0-6.657,8.033-10.16,8.033c0,0,0-8.217,5.606-8.217c-5.606,0-5.606-8.217-5.606-8.217
+						    c3.504,0,10.16,8.033,10.16,8.033c15.941,18.904,27.911,2.493,27.911,2.493S204.937,363.309,205.093,362.893z"
+                    ></path>
+                    <circle
+                        id="fishEye"
+                        style="
+                            fill: none;
+                            stroke-miterlimit: 10;
+                            transition: all 2s ease 0s;
+                        "
+                        class="pre-unique"
+                        cx="200.48"
+                        cy="360.556"
+                        r="1.532"
+                    ></circle>
+                </svg>
             </div>
         </div>
         <div class="fish-text">
@@ -33,8 +75,44 @@ import { RefValue } from 'unplugin-vue2-script-setup/ref-macros';
 
 const { proxy: _this } = getCurrentInstance() as ComponentInternalInstance;
 
-const setClass = (index: number): string => {
+// 设置所有鱼的显示隐藏
+let showAllFish: RefValue<boolean> = $ref(false);
+// 修改普通鱼的class
+let showCommon: RefValue<boolean> = $ref(true);
+const setClass = (index: number) => {
+    if (showAllFish === true) {
+        return `el-${index} el-show`;
+    }
     return `el-${index}`;
+};
+
+// 修改特殊鱼的class
+const setSpecialFish = (fishRef: any[]) => {
+    setTimeout(() => {
+        fishRef[0].children[0].classList.add('unique');
+        fishRef[0].children[1].classList.add('unique');
+        setTimeout(() => {
+            fishRef[0].classList.add('big');
+            setTimeout(() => {
+                fishRef[0].classList.add('stand-apart');
+                setTimeout(() => {
+                    showCommon = false;
+                    fishRef[0].classList.add('you');
+                    setTimeout(() => {
+                        fishRef[0].classList.remove(
+                            'big',
+                            'stand-apart',
+                            'you',
+                        );
+                        console.log(fishRef[0].children[0].classList);
+                        fishRef[0].children[0].classList.remove('unique');
+                        fishRef[0].children[1].classList.remove('unique');
+                        showCommon = true;
+                    }, 3000);
+                }, 3000);
+            }, 3000);
+        }, 3000);
+    }, 3000);
 };
 
 const title = [
@@ -44,10 +122,12 @@ const title = [
     'we make you stand apart',
     'we make you ',
 ];
-// 让字体循环变换
+// 让字体循环变换及控制所有鱼的出现
 let i: RefValue<number> = $ref(-1);
 const changeText = () => {
     setTimeout(() => {
+        // 展示所有鱼图标
+        showAllFish = true;
         if (i === -1) {
             i++;
         }
@@ -63,6 +143,8 @@ const changeText = () => {
 
 onMounted(() => {
     changeText();
+    const specialFishRef: any = _this.$refs['specialFish'];
+    setSpecialFish(specialFishRef);
 });
 </script>
 
@@ -91,18 +173,64 @@ onMounted(() => {
         justify-content: center;
         align-items: flex-end;
         margin: 0 7px;
+        opacity: 0;
         transition: all 2s ease-in-out;
 
         &-4 {
-            // translate: ;
+            .special {
+                position: absolute;
+                width: 48px;
+                bottom: 0;
+                left: 50%;
+                transform: translateX(-50%);
+                background: url('@/assets/img/fish.svg');
+                transition: all 2s ease-in-out;
+            }
+
+            .pre-unique {
+                stroke: #d1d1d1;
+                transition: all 2s ease-in-out;
+            }
+
+            .unique {
+                stroke: rgb(255, 204, 0);
+            }
+
+            .big {
+                background: url('');
+                width: 85px;
+                left: 50%;
+                transform: translateX(-50%);
+                bottom: 0;
+            }
+
+            .stand-apart {
+                left: 50%;
+                transform: translateX(-50%);
+                bottom: 20px;
+            }
+
+            .you {
+                left: -60%;
+                bottom: -80px;
+            }
         }
 
         &-6 {
             margin-left: 14px;
         }
 
+        &-show {
+            opacity: 1;
+        }
+
+        &-other-off {
+            opacity: 0;
+        }
+
         img {
-            width: 40px;
+            width: 48px;
+            transition: all 2s ease-in-out;
         }
     }
 }
